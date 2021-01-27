a42fea37af39c6cbb159721a0526f1e0ce3e4518
Niccolò Cuartas <cuartas.niccolo@gmail.com>
2021-01-27 17:33:14 +0100

    Aggiunta dei nuovi enum previsti dal modello di dominio

--- Java/backend/src/main/java/Categoria.java
+++ Java/backend/src/main/java/Categoria.java
@@ -1,4 +1,3 @@
-import com.sun.org.apache.xpath.internal.operations.Mod;
 
 public enum Categoria {
     //tutte le categorie di prodotto
---
+++ Java/backend/src/main/java/Veicolo.java
@@ -0,0 +1,16 @@
+/**
+ * Classificazione del tipo di veicolo prevista (e relativa compatibilità con dimensioni della spedizione)
+ *
+ * @Veicolo#Leggero : Bicicletta, Motorino, Scooter, Moto, Smartcar
+ *
+ * @Veicolo#Medio: Automobile, Furgoncino
+ *
+ * @Veicolo#Pesante: Furgone, Camion, Trattori Stradali e superiori
+ *
+ * @author Niccolò Cuartas
+ */
+public enum Veicolo {
+    Leggero,
+    Medio,
+    Pesante
+}
